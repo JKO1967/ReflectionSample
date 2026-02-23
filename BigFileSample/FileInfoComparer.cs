16b8f03b6b17c5fa77072fc948421f39e53fdd84
Jürgen Kotz <juergen.kotz@primetime-software.de>
2026-02-23 15:42:05 +0100

    BigFileSample

---
+++ BigFileSample/FileInfoComparer.cs
@@ -0,0 +1,11 @@
+﻿namespace BigFileSample;
+public class FileInfoComparer : IComparer<FileInfo>
+{
+    public int Compare(FileInfo? x, FileInfo? y)
+    {
+        if (x == null && y == null) return 0;
+        if (x == null) return -1;
+        if (y == null) return 1;
+        return y.Length.CompareTo(x.Length);
+    }
+}
---
+++ BigFileSample/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace BigFileSample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\Windows";
+
+        ShowLargeFilesWithoutLinq(path);
+        Console.WriteLine("----------------------------------------------------------------------");
+        ShowLargeFilesWithLinq(path);
+    }
+
+    private static void ShowLargeFilesWithLinq(string path)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+        var files = directoryInfo.GetFiles()
+            .OrderByDescending(f => f.Length)
+            .Take(5);
+
+        foreach (var file in files)
+        {
+            Console.WriteLine($"{file.FullName} ist {file.Length} Bytes groß!");
+        }
+    }
+
+    private static void ShowLargeFilesWithoutLinq(string path)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+        FileInfo[] files = directoryInfo.GetFiles();
+
+        Array.Sort(files, new FileInfoComparer());
+
+        int length = 5;
+
+        if (files.Length < length)
+        {
+            length = files.Length;
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine($"{files[i].FullName} ist {files[i].Length} Bytes groß!");
+        }
+    }
+}
