aa1451fc1d38fa5c604de96a0580bd3c6bb4be0d
Jürgen Kotz <juergen.kotz@primetime-software.de>
2026-02-25 10:32:09 +0100

    Thread und BlockingCollection

---
+++ BlockingCollectionSample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+
+namespace BlockingCollectionSample;
+
+internal class Program
+{
+    private static BlockingCollection<string> _products = new BlockingCollection<string>();
+
+    static void Main(string[] args)
+    {
+        // ThreadPool.QueueUserWorkItem(Produce);
+        Task produce = Task.Run(Produce);
+        Task consume = Task.Run(Consume);
+
+        // Hauptprogramm wird blockiert bis beide tasks fertig sind
+        Task.WaitAll(produce, consume);
+    }
+
+
+    static async Task Produce()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            _products.Add($"Product {i}");
+            Console.WriteLine($"Product {i} wurde erstellt! fertig zum Verkauf!");
+            await Task.Delay(50);
+            if (i % 10 == 0)
+            {
+                await Task.Delay(2000);
+            }
+        }
+        // Befüllen der Liste beendet
+        _products.CompleteAdding();
+        Console.WriteLine("Feierabend");
+    }
+
+    static async Task Consume()
+    {
+        foreach (var item in _products.GetConsumingEnumerable())
+        {
+            Console.WriteLine($"{item} wurde verkauft");
+            await Task.Delay(50);
+        }
+
+        Console.WriteLine("Verkauf beendet");
+    }
+}
---
+++ ThreadingSample/Program.cs
@@ -0,0 +1,55 @@
+﻿
+namespace ThreadingSample;
+
+internal class Program
+{
+    private static bool _printed = false;
+    private static object _lock = new object();
+    private static int result = 0;
+
+    static void Main(string[] args)
+    {
+        Thread t1 = new Thread(PrintOnce);
+        t1.Name = "Thread 1";
+        //t1.IsBackground = true;
+        Thread t2 = new Thread(PrintOnce);
+        t2.Name = "Thread 2";
+        //t2.IsBackground = true;
+
+        t1.Start();
+        t2.Start();
+        // t1.Join();
+
+        // optimierteste Version einen Background Thread zu erstellen (mir fehlt aber dann die Kontrolle)
+        //ThreadPool.QueueUserWorkItem(PrintOnceBackground);
+    }
+
+    private static void PrintOnceBackground(object? state)
+    {
+        PrintOnce();
+    }
+
+    private static void PrintOnce()
+    {
+        lock (_lock)
+        {
+            //Monitor.Enter(_lock);
+
+            if (!_printed)
+            {
+                Console.WriteLine($"Print in Thread {Thread.CurrentThread.Name} at {DateTime.Now.ToLongTimeString()}");
+                _printed = true;
+            }
+        }
+
+        for (int i = 0; i < 100_000_000; i++)
+        {
+            //result++;
+            Interlocked.Increment(ref result);
+        }
+
+        Console.WriteLine(result);
+        Console.WriteLine($"Thread {Thread.CurrentThread.Name} at {DateTime.Now.ToLongTimeString()}");
+        //Monitor.Exit(_lock);
+    }
+}
